192684211b7edaeee1f155e123707a2a49879db7
zeluspudding <agostiniworks@gmail.com>
2015-09-25 18:49:25 +0000

    Properly indent file

--- tempmeter_es.py
+++ tempmeter_es.py
@@ -6,17 +6,16 @@ es = Elasticsearch([{'host': '104.131.22.155', 'port': 9200}])
 if __name__ == '__main__':
     try:
         i = 1 #Prepare to iterate over samples
-
-while True:
-    temperature = getCPUtemperature()
-    data = {'message': 'temperature %.1f cm' % temperature, 'temperature': temperature, 'hostname': socket.gethostname()}
-    print ("Received temperature = %.1f C" % temperature)
-    es.index(index='raspi1', doc_type='stimuli', id=i, body=json.loads(data))
-    es.index(index='raspi2', doc_type='stimuli', id=i, body=json.dumps(data))
-    es.index(index='raspi3', doc_type='stimuli', id=i, body=data)
-    i=i+1
-    time.sleep(0.2)
+        while True:
+            temperature = getCPUtemperature()
+            data = {'message': 'temperature %.1f cm' % temperature, 'temperature': temperature, 'hostname': socket.gethostname()}
+            print ("Received temperature = %.1f C" % temperature)
+            es.index(index='raspi1', doc_type='stimuli', id=i, body=json.loads(data))
+            es.index(index='raspi2', doc_type='stimuli', id=i, body=json.dumps(data))
+            es.index(index='raspi3', doc_type='stimuli', id=i, body=data)
+            i=i+1
+            time.sleep(0.2)
     
     # interrupt
-except KeyboardInterrupt:
-    print("Programm interrupted")
+    except KeyboardInterrupt:
+        print("Programm interrupted")
