b0a492f994cc2cb234918ba5dd11cfd2b19885a0
zeluspudding <agostiniworks@gmail.com>
2015-09-25 22:36:09 +0000

    Host address update - don't steal my server please

--- tempmeter_es.py
+++ tempmeter_es.py
@@ -1,17 +1,18 @@
 import socket, json, time
+from datetime import datetime
 from cputempmeter import getCPUtemperature
 from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': '104.131.22.155', 'port': 9200}])
+es = Elasticsearch([{'host': '159.203.74.153', 'port': 9200}])
 
 if __name__ == '__main__':
     try:
         i = 1 #Prepare to iterate over samples
         while True:
             temperature = getCPUtemperature()
-            data = {'message': 'temperature %.1f cm' % temperature, 'temperature': temperature, 'hostname': socket.gethostname()}
+            data = {'temperature': temperature, 'hostname': socket.gethostname(), "timestamp": datetime.now()}
             print ("Received temperature = %.1f C" % temperature)
-            es.index(index='raspi1', doc_type='stimuli', id=i, body=json.loads(data))
-            es.index(index='raspi2', doc_type='stimuli', id=i, body=json.dumps(data))
+            #es.index(index='raspi1', doc_type='stimuli', id=i, body=json.loads(data))
+            #es.index(index='raspi1', doc_type='stimuli', id=i, body=json.dumps(data))
             es.index(index='raspi3', doc_type='stimuli', id=i, body=data)
             i=i+1
             time.sleep(0.2)
